897b7ff9512a61b1754693f0aaebe45eb499c1e8
Antonio González Oliva <gozaol@gmail.com>
2024-04-19 07:57:31 +0200

    primer commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *
+ * @author Antonio González Oliva
+ * @version 1.0
+ * @internal Comentario para el desarrollador
+*/
+
+/**
+ *
+ * Esta función realiza la multiplicación de dos números que se pasan por parámetros y devuelve el resultado de la misma.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La multiplicación de los dos números.
+ *
+ */
+
+function multiplicar($a, $b) {
+    return $a * $b;
+}
+
+/**
+ *
+ * Esta función realiza la división de dos números que se pasan por parámetros y devuelve el resultado de la misma.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La división de los dos números.
+ * 
+ */
+
+function dividir($a, $b) {
+    return $a / $b;
+}
+
+
+?>
